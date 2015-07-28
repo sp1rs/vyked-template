192772603c1ae0a692682c1966b4974d5523360a
Shashank Parekh <shashankparekh@TECH-SHASHANKPAREKH.local>
2015-07-28 17:56:14 +0530

    Minor change in service

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/service.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/service.py
@@ -4,25 +4,23 @@ from again.decorate import silence_coroutine
 from aiohttp.web import Response
 
 
-class ComboHTTPService(HTTPApplicationService):
+class HTTPService(HTTPApplicationService):
 
     def __init__(self, host, port):
-        self._manager = ComboManager()
         super(ComboHTTPService, self).__init__('{{cookiecutter.service_name}}', 1, host, port)
 
 
 
-class CombosTCPService(TCPApplicationService):
+class TCPService(TCPApplicationService):
 
     def __init__(self, host, port):
-        self._manager = ComboManager()
         super(CombosTCPService, self).__init__('{{cookiecutter.service_name}}', 1, host, port)
 
 
 if __name__ == '__main__':
     bus = Bus()
-    tcp_service = CombosTCPService('{{cookiecutter.tcp_address}}', {{cookiecutter.tcp_port}})
-    http_service = ComboHTTPService('{{cookiecutter.http_address}}', {{cookiecutter.http_port}})
+    tcp_service = TCPService('{{cookiecutter.tcp_address}}', {{cookiecutter.tcp_port}})
+    http_service = HTTPService('{{cookiecutter.http_address}}', {{cookiecutter.http_port}})
     bus.serve_tcp(tcp_service)
     bus.serve_http(http_service)
     bus.start('127.0.0.1', 4500, '127.0.0.1', 6379)
